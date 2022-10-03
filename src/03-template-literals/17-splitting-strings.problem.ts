abf5a647300497d3eedb79a601284a5833ae9af8
Pakapon Kaewmark <pakapon.k@skooldio.com>
2022-10-03 09:38:39 +0700

    Solve Exercise 17 - Template Literal Type - Split via `ts-toolbelt`

--- src/03-template-literals/17-splitting-strings.problem.ts
+++ src/03-template-literals/17-splitting-strings.problem.ts
@@ -2,11 +2,12 @@
 // import { S } from "ts-toolbelt";
 // https://millsp.github.io/ts-toolbelt/modules/string_split.html
 
+import { Split } from "ts-toolbelt/out/String/Split";
 import { Equal, Expect } from "../helpers/type-utils";
 
 type Path = "Users/John/Documents/notes.txt";
 
-type SplitPath = unknown;
+type SplitPath = Split<Path, "/">;
 
 type tests = [
   Expect<Equal<SplitPath, ["Users", "John", "Documents", "notes.txt"]>>,
